f0e853c17a1a82d9cc64132e40229a5cfbae04ff
mean pt <meanpt@gmail.com>
2017-12-15 17:47:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("I hope" , "the tests", "will break!");
+console.error("You did it wrong, really wrong","Did I mention the error?")
+console.warn("Lookout! You're in the wrong direction!");
